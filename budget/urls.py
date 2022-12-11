276568be07d8db1ada2dffa451ad5f56013c3235
Afiyaayat <37040878+sohelranacse@users.noreply.github.com>
2022-12-11 15:11:51 +0600

    update version

--- budget/urls.py
+++ budget/urls.py
@@ -13,9 +13,9 @@ urlpatterns = [
     url('check_cookie_name', views.check_cookie_name, name='check_cookie_name'),
     url('search_date_wise_expense', views.search_date_wise_expense, name='search_date_wise_expense'),
 
-    # url('budget', budgetViews.budget, name='budget'),
-    # url('search_month_wise_expense', budgetViews.search_month_wise_expense, name='search_month_wise_expense'),
+    url('budget', budgetViews.budget, name='budget'),
+    url('search_month_wise_expense', budgetViews.search_month_wise_expense, name='search_month_wise_expense'),
 
-    # url('comparison', comparisonViews.comparison, name='comparison'),
-    # url('search_expense', comparisonViews.search_expense, name='search_expense'),
+    url('comparison', comparisonViews.comparison, name='comparison'),
+    url('search_expense', comparisonViews.search_expense, name='search_expense'),
 ]
